5b259d56cac4937d8969d5a19d4fd6e816d30e22
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2025-03-10 16:30:50 -0500

    Add header file for I2C ADC driver

--- software/apps/pianostar/i2c_adc.c
+++ software/apps/pianostar/i2c_adc.c
@@ -1,9 +1,19 @@
-#include "nrf_twi_mngr.h"
+#include "i2c_adc.h"
 
-static const uint8_t ADC_ADDRESS_0 = 0x48;
-static const uint8_t ADC_ADDRESS_1 = 0x49;
-static const uint8_t ADC_ADDRESS_2 = 0x4a;
-static const uint8_t ADC_ADDRESS_3 = 0x4b;
+// Pointer to an initialized I2C instance to use for transactions
+static const nrf_twi_mngr_t *i2c_manager = NULL;
+
+static const uint8_t ADC_ADDRESS[4] = {
+    0x48,
+    0x49,
+    0x4a,
+    0x4b,
+};
+
+static const uint8_t CHANNELS[8] = {
+    0b000 << 4, 0b100 << 4, 0b001 << 4, 0b101 << 4,
+    0b010 << 4, 0b110 << 4, 0b011 << 4, 0b111 << 4,
+};
 
 // COMMAND BYTE
 // MSB ---------------- LSB
@@ -11,41 +21,30 @@ static const uint8_t ADC_ADDRESS_3 = 0x4b;
 // - SD: single ended
 // - PD: 1 1
 static const uint8_t COMMAND_BASE = 0b10001100;
-// Pointer to an initialized I2C instance to use for transactions
-static const nrf_twi_mngr_t* i2c_manager = NULL;
 
-static const uint8_t CHANNELS[8] = {
-    0b000 << 4,
-    0b100 << 4,
-    0b001 << 4,
-    0b101 << 4,
-    0b010 << 4,
-    0b110 << 4,
-    0b011 << 4,
-    0b111 << 4,
-};
+uint8_t i2c_adc_read(uint8_t adc_idx, uint8_t adc_channel_idx) {
+  uint8_t adc_address = ADC_ADDRESS[adc_idx];
+  uint8_t command_byte = COMMAND_BASE | CHANNELS[adc_channel_idx];
+  uint8_t rx_buf = 0;
 
-uint8_t read_adc(uint8_t adc_address, uint8_t adc_channel)
-{
-    uint8_t command_byte = COMMAND_BASE | CHANNELS[adc_channel];
-    uint8_t rx_buf = 0;
+  nrf_twi_mngr_transfer_t const read_transfer[] = {
+      NRF_TWI_MNGR_WRITE(adc_address, &command_byte, 1, NRF_TWI_MNGR_NO_STOP),
+      NRF_TWI_MNGR_READ(adc_address, &rx_buf, 1, 0)};
 
-    nrf_twi_mngr_transfer_t const read_transfer[] = {
-        NRF_TWI_MNGR_WRITE(adc_address, &command_byte, 1, NRF_TWI_MNGR_NO_STOP),
-        NRF_TWI_MNGR_READ(adc_address, &rx_buf, 1, 0)
-    };
+  ret_code_t result =
+      nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 2, NULL);
+  if (result != NRF_SUCCESS) {
+    // Likely error codes:
+    //  NRF_ERROR_INTERNAL            (0x0003) - something is wrong with the
+    //  driver itself NRF_ERROR_INVALID_ADDR        (0x0010) - buffer passed was
+    //  in Flash instead of RAM NRF_ERROR_BUSY                (0x0011) - driver
+    //  was busy with another transfer still NRF_ERROR_DRV_TWI_ERR_OVERRUN
+    //  (0x8200) - data was overwritten during the transaction
+    //  NRF_ERROR_DRV_TWI_ERR_ANACK   (0x8201) - i2c device did not acknowledge
+    //  its address NRF_ERROR_DRV_TWI_ERR_DNACK   (0x8202) - i2c device did not
+    //  acknowledge a data byte
+    printf("I2C transaction failed! Error: %lX\n", result);
+  }
 
-    ret_code_t result = nrf_twi_mngr_perform(i2c_manager, NULL, read_transfer, 2, NULL);
-    if (result != NRF_SUCCESS) {
-        // Likely error codes:
-        //  NRF_ERROR_INTERNAL            (0x0003) - something is wrong with the driver itself
-        //  NRF_ERROR_INVALID_ADDR        (0x0010) - buffer passed was in Flash instead of RAM
-        //  NRF_ERROR_BUSY                (0x0011) - driver was busy with another transfer still
-        //  NRF_ERROR_DRV_TWI_ERR_OVERRUN (0x8200) - data was overwritten during the transaction
-        //  NRF_ERROR_DRV_TWI_ERR_ANACK   (0x8201) - i2c device did not acknowledge its address
-        //  NRF_ERROR_DRV_TWI_ERR_DNACK   (0x8202) - i2c device did not acknowledge a data byte
-        printf("I2C transaction failed! Error: %lX\n", result);
-    }
-
-    return rx_buf;
+  return rx_buf;
 }
---
+++ software/apps/pianostar/i2c_adc.h
@@ -0,0 +1,6 @@
+#include "nrf_twi_mngr.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+uint8_t i2c_adc_read(uint8_t adc_idx, uint8_t adc_channel_idx);
