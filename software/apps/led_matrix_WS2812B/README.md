32e3642ff4a49db4306d772610cb52c8c725f041
Amy Liao <amzhliao@gmail.com>
2025-03-12 13:17:31 -0500

    led matrix starter

---
+++ software/apps/led_matrix_WS2812B/README.md
@@ -0,0 +1,6 @@
+LED Matrix WS2812B
+==============
+
+Piano Star 
+
+Code for operating the WS2812B LED matrix.
---
+++ software/apps/led_matrix_WS2812B/led_matrix.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+// Initialize the LED matrix display
+void led_matrix_init(void);
+
+// You may need to add more functions here
