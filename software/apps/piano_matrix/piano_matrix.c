af31cd08c9afcbd4df807f7a6f1f2efd9cfd2f85
Amy Liao <amzhliao@gmail.com>
2025-03-14 16:04:31 -0500

    inits done

--- software/apps/piano_matrix/piano_matrix.c
+++ software/apps/piano_matrix/piano_matrix.c
@@ -13,17 +13,31 @@
 
 uint32_t OUTPUT_PIN = 15;
 
+APP_TIMER_DEF(timer1);
+
 uint32_t PERIOD_TICKS = 1000;
+uint32_t ONE_BIT_TICKS = 500;
+uint32_t ZERO_BIT_TICKS = 100;
 
 void led_matrix_init(void)
 {
   // set gpio pin to output
   nrf_gpio_pin_dir_set(OUTPUT_PIN, NRF_GPIO_PIN_DIR_OUTPUT);
+  nrf_gpio_pin_clear(OUTPUT_PIN);
+}
+
+void drive_high(void)
+{
+  // set gpio pin high
+  nrf_gpio_pin_write(OUTPUT_PIN, 1);
 }
 
 void timer_init(void)
 {
   // set up new timer to drive pin high every X ticks
+  app_timer_init();
+  app_timer_create(&timer1, APP_TIMER_MODE_REPEATED, drive_high);
+  app_timer_start(timer1, PERIOD_TICKS, NULL);
 }
 
 void write_bit(uint8_t bit)
