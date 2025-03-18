fdf4b71632474614ec81ecd0d83352556234fb98
Amy Liao <amzhliao@gmail.com>
2025-03-18 15:16:23 -0500

    lab5

--- software/apps/pwm_square_tone/main.c
+++ software/apps/pwm_square_tone/main.c
@@ -20,56 +20,68 @@ nrf_pwm_values_common_t sequence_data[1] = {0};
 
 // Sequence structure for configuring DMA
 nrf_pwm_sequence_t pwm_sequence = {
-  .values.p_common = sequence_data,
-  .length = 1,
-  .repeats = 0,
-  .end_delay = 0,
+    .values.p_common = sequence_data,
+    .length = 1,
+    .repeats = 0,
+    .end_delay = 0,
 };
 
-
-static void pwm_init(void) {
+static void pwm_init(void)
+{
   // Initialize the PWM
   // SPEAKER_OUT is the output pin, mark the others as NRFX_PWM_PIN_NOT_USED
   // Set the clock to 500 kHz, count mode to Up, and load mode to Common
   // The Countertop value doesn't matter for now. We'll set it in play_tone()
-  // TODO
+  nrfx_pwm_config_t pwm_config = {
+      .output_pins = {SPEAKER_OUT, NRFX_PWM_PIN_NOT_USED, NRFX_PWM_PIN_NOT_USED, NRFX_PWM_PIN_NOT_USED},
+      .base_clock = NRF_PWM_CLK_500kHz,
+      .count_mode = NRF_PWM_MODE_UP,
+      .top_value = 100, // anything for now
+      .load_mode = NRF_PWM_LOAD_COMMON,
+      .step_mode = NRF_PWM_STEP_AUTO};
+
+  nrfx_pwm_init(&PWM_INST, &pwm_config, NULL);
 }
 
-static void play_tone(uint16_t frequency) {
+static void play_tone(uint16_t frequency)
+{
   // Stop the PWM (and wait until its finished)
-  // TODO
+  nrfx_pwm_stop(&PWM_INST, true);
 
   // Set a countertop value based on desired tone frequency
   // You can access it as NRF_PWM0->COUNTERTOP
-  // TODO
+  NRF_PWM0->COUNTERTOP = 500000 / frequency;
 
   // Modify the sequence data to be a 25% duty cycle
-  // TODO
+  sequence_data[0] = 0.5 * 500000 / frequency;
 
   // Start playback of the samples and loop indefinitely
-  // TODO
+  nrfx_pwm_simple_playback(&PWM_INST, &pwm_sequence, frequency, NRFX_PWM_FLAG_STOP);
 }
 
-
-int main(void) {
+int main(void)
+{
   printf("Board started!\n");
 
   // initialize PWM
   pwm_init();
 
   // Play the A4 tone for one second
-  // TODO
+  play_tone(440);
+  nrf_delay_ms(1000);
 
   // Play the C#5 tone for one second
-  // TODO
+  play_tone(554);
+  nrf_delay_ms(1000);
 
   // Play the E5 tone for one second
-  // TODO
+  play_tone(659);
+  nrf_delay_ms(1000);
 
   // Play the A5 tone for one second
-  // TODO
+  play_tone(880);
+  nrf_delay_ms(1000);
 
   // Stop all noises
-  // TODO
+  nrfx_pwm_stop(&PWM_INST, true);
 }
-
